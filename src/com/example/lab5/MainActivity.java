86b6316b3840813dfb53fd8a6ba0bc4482ca9b06
Stefan Gabriela <gabriela28.stefan@gmail.com>
2016-03-31 23:17:43 +0300

    lab5v2

--- src/com/example/lab5/MainActivity.java
+++ src/com/example/lab5/MainActivity.java
@@ -4,16 +4,66 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
 
+	EditText editText;
+	Button buton1;
+	Button buton2;
+	Button buton3;
+	
+	TextView text1;
+	TextView text2;
+
+	
+	private ButtonListener buttonListener = new ButtonListener();
+	private class ButtonListener implements View.OnClickListener {
+
+		@Override
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case R.id.button1:
+				if (editText.getText() != null) {
+					int val = Integer.parseInt(editText.getText().toString());
+					val++;
+					text1.setText(String.valueOf(val));
+				}
+				break;
+			case R.id.button2:
+				int val2 = Integer.parseInt(text2.getText().toString());
+				++val2;
+				text2.setText(String.valueOf(val2));
+				break;
+			default:
+				break;
+			}
+		}
+		
+	}
+	
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText editText = (EditText) findViewById(R.id.edit);
+         editText = (EditText) findViewById(R.id.edit1);
+         editText.setText("0");
+         
+         buton1 = (Button) findViewById(R.id.button1);
+         buton2 = (Button) findViewById(R.id.button2);
+         buton3 = (Button) findViewById(R.id.button3);
+         
+         buton1.setOnClickListener(buttonListener);
+         buton2.setOnClickListener(buttonListener);
+         
+         text1 = (TextView) findViewById(R.id.text1);
+         text2 = (TextView) findViewById(R.id.text2);
+         text2.setText("0");
     }
 
     @Override
